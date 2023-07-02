129d7ff38b3a478410c48c575c961d21b759cd5a
tukwan <nick@atlhk.net>
2023-07-02 16:07:53 +0700

    fix docker next envs

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request"
 import { getSdk } from "./gql"
 
 const gqlClient = new GraphQLClient(
-  process.env.NEXT_PUBLIC_GRAPHQL_API_URL || ""
+  process.env.GRAPHQL_API_URL || process.env.NEXT_PUBLIC_GRAPHQL_API_URL || ""
 )
 
 export const { getProjects, getProjectById, createProject } = getSdk(gqlClient)
